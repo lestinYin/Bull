4e5a3aa22dd8437da48ad3eb9f7e9862a2d9b483
lestin <yinmaolin8@163.com>
2019-07-17 19:34:43 +0800

    第一次提交

---
+++ app/src/main/java/com/future/taurus/ASplash.kt
@@ -0,0 +1,12 @@
+package com.future.taurus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ASplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asplash)
+    }
+}
